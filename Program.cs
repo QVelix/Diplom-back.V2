00aefba1485ebdb3eaff3ca3e7baec581331c4d0
Максим Богачёв <m.bogachev@solo-it.ru>
2023-05-30 01:55:31 +0400

    Добавление использования MySql

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System.Configuration;
+using Diplom_back.Database;
+using Microsoft.EntityFrameworkCore;
+using Diplom_back.Models;
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Diplom_backContext>(opt =>
+    opt.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")
+    )
+);
 
 var app = builder.Build();
 
